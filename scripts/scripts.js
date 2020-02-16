df4acc969076e742a41e83a5aa138ccf3055b91c
theFl00f <celeste.ellerby@gmail.com>
2020-02-15 22:01:52 -0500

    dogApp initialized, jQuery referenced

---
+++ scripts/scripts.js
@@ -0,0 +1,41 @@
+
+
+//
+//
+//
+/////// which good boi r u?
+//
+//
+// by char and celeste c:
+
+
+
+//VARIBLES
+
+
+
+
+
+
+//create app namespace to hold all methods
+const dogApp = {};
+
+//***NEEDS EDIT, place logic here c: ---create object with methods nested within. the name of each method is based on "answers" with dogs, finally, in an array of last question
+
+//collect user input "answer" in an array.
+
+//on click of button, check if array contains answers required for each dog
+
+//store dog breed based on "answer" within variable
+//make AJAX request with final dog breed.
+
+//display image on the page based on what dog's breed is.
+
+//start app
+dogApp.init = () => {
+    console.log('app initialized')
+}
+
+$(() => {
+    dogApp.init();
+})
