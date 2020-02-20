7968f9c32b49dd20707024ece672dc21c4d78309
poncechar <poncechar@gmail.com>
2020-02-20 10:25:37 -0500

    added new alert styling and doge blurbs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,9 +24,15 @@ const $questionText = $('.questionContainer p');
 
 const $answerContainer = $('.answerContainer');
 
-const $selectedInput = $('input[type=radio]:checked')
+const $selectedInput = $('input[type=radio]:checked');
+
+const $dogeImg = $('.imgContainer img');
+
+const $dogeBlurb = $('.resultsPage p');
 
 const $input = $('input')
+
+
 
 let questionOneAnswer;
 
@@ -84,29 +90,28 @@ const showResult = () => {
 
     if (userInput.question1 === true && userInput.question2 === true && userInput.question5 === true) {
         userBreed = dogOptions[6];
-        console.log(userBreed);
+        $dogeBlurb.html(`Pitbulls are often grossly misunderstood. They are loving and gentle with people and often makes a lousy guard dog because of his tail-wagging eagerness to greet the person at the door. They have a zest for life and love to be involved in everything going on around them.`);
     } else if (userInput.question1 === true && userInput.question2 === true && userInput.question5 === false) {
         userBreed = dogOptions[7];
-        console.log(userBreed);
+        $dogeBlurb.html(`Small in size but big in personality, the Yorkshire Terrier makes a feisty but loving companion. Oblivious to their small size, the Yorkshire Terrier is a big dog in a small body, always on the lookout for adventure and maybe even a bit of trouble.`);
     } else if (userInput.question1 === true && userInput.question2 === false && userInput.question4 === true) {
         userBreed = dogOptions[5];
-        console.log(userBreed);
+        $dogeBlurb.html(`The Bichon Frise is a cheerful, small dog breed with a love of mischief and a lot of love to give. They love to play and are always happy (except when left alone for long periods of time), and their demeanor is affectionate and gentle.And it doesn’t take long to realize that the Bichon can be your happiest and most enthusiastic companion.`);
     } else if (userInput.question1 === true && userInput.question2 === false && userInput.question4 === false) {
         userBreed = dogOptions[4];
-        console.log(userBreed);
-
+        $dogeBlurb.html(`Cute, feisty and furry, Poms are intelligent and loyal to their families. Don't let their cuteness fool you, however. These independent, bold dogs have minds of their own. They are alert and curious about the world around them. Unfortunately, in their minds, they are much larger than they really are, which can sometimes lead them to harass and even attack much larger dogs.`);
     } else if (userInput.question1 === false && userInput.question3 === true && userInput.question4 === true) {
         userBreed = dogOptions[3];
-        console.log(userBreed);
+        $dogeBlurb.html(`The Shiba Inu is known for a bold, fiery personality. The Japanese have three words to describe the breed's mental traits: kaani-i (spirited boldness), ryosei (good nature), and soboku (alertness). Combined, these traits make up the interesting, intelligent, and strong-willed temperament.`);
     } else if (userInput.question1 === false && userInput.question3 === true && userInput.question4 === false) {
         userBreed = dogOptions[2];
-        console.log(userBreed);
+        $dogeBlurb.html(`The Bernese Mountain Dog is a large and sturdy dog breed, with a friendly and calm disposition. They're known for being loyal, affectionate, eager to please, and intelligent. Most of all, they have a happy-go-lucky attitude about life.`);
     } else if (userInput.question1 === false && userInput.question3 === false && userInput.question5 === true) {
         userBreed = dogOptions[1];
-        console.log(userBreed);
+        $dogeBlurb.html(`Mastiffs are kind, dignified, and courageous dogs who love being with people. Although the Mastiff's size makes them appear fierce, their temperament is one of good-natured docility. They prefer the comforts of home and the presence of their family and will do their best to be a lap dog- or at least a cushy footstool.`);
     } else {
         userBreed = dogOptions[0];
-        console.log(userBreed);
+        $dogeBlurb.html(`The Chihuahua is a saucy little hot tamale and not just because of their association with a certain fast-food Mexican restaurant. They are renowned for being the world's smallest dog, but may well have the world's biggest personality stashed inside that tiny body.`);
     }
 
     resultBreed = userBreed;
@@ -142,6 +147,7 @@ $('button').on('click', (e) => {
         userInput.question1 = true;
         $questionText.removeClass('true false')
         $questionText.html(`when you wake up, what's your hair like?`)
+        // $dogeImg.attr('src', 'assets/icons-doges/corgi.png')
         $answerContainer.first().children().html(`wake up and go!`);
         $answerContainer.last().children().html(`you tHINK you've seen bedhead`);
         $questionText.removeClass('question1');
@@ -215,6 +221,11 @@ $input.on('click', () => {
     $questionText.addClass(questionOneAnswer)
 });
 
+$('.next').on('click', () => {
+    if ($input.is(":not(':checked')")) {
+        swal('oops 💩', 'henlo fren, plz choose an option!');
+    }
+})
 
 //make AJAX request with final dog breed.
 dogApp.getFinalDog = () => {
@@ -232,7 +243,7 @@ dogApp.getFinalDog = () => {
 //display image on the page based on what dog's breed is.
 
 dogApp.displayResult = () => {
-    $resultImg.attr('src', imgToAppend).attr('alt', resultBreed)
+    $resultImg.attr('src', imgToAppend).attr('alt', resultBreed);
 }
 
 
