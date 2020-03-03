ebe42a5a478a6a4b3803c92f54f37866eea59606
poncechar <poncechar@gmail.com>
2020-03-03 14:20:15 -0500

    Updates

---
+++ precache-manifest.7ccade80a78b08479bac08343f5a5432.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5d7c59591fa217cde1acdd25974378",
+    "url": "/ponce-char-project-5/index.html"
+  },
+  {
+    "revision": "695496d556abbde3fbb5",
+    "url": "/ponce-char-project-5/static/css/main.d463e39e.chunk.css"
+  },
+  {
+    "revision": "ccc68b55dc99da02933d",
+    "url": "/ponce-char-project-5/static/js/2.cec8dd80.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/ponce-char-project-5/static/js/2.cec8dd80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695496d556abbde3fbb5",
+    "url": "/ponce-char-project-5/static/js/main.3eac6ea3.chunk.js"
+  },
+  {
+    "revision": "2d2ff1db4d990ad61f39",
+    "url": "/ponce-char-project-5/static/js/runtime-main.3b1a5fd2.js"
+  },
+  {
+    "revision": "05dc1e3bb53e44f2194df5d65e9647c6",
+    "url": "/ponce-char-project-5/static/media/chlorophytum.05dc1e3b.png"
+  },
+  {
+    "revision": "9af512feba3e240f6c36a34cad3dd31f",
+    "url": "/ponce-char-project-5/static/media/fertilizer.9af512fe.png"
+  },
+  {
+    "revision": "0e32ec777418d496a5b3c7586f33d3a2",
+    "url": "/ponce-char-project-5/static/media/location.0e32ec77.png"
+  },
+  {
+    "revision": "6a7e8e67486973229b64a1a134e5df0d",
+    "url": "/ponce-char-project-5/static/media/mist.6a7e8e67.png"
+  },
+  {
+    "revision": "872e4d336cb650b95517dd1899075e9c",
+    "url": "/ponce-char-project-5/static/media/monstera.872e4d33.png"
+  },
+  {
+    "revision": "0309857151fe5fffbf66db70af18034b",
+    "url": "/ponce-char-project-5/static/media/pots.03098571.png"
+  },
+  {
+    "revision": "46420a8e9ca6283d8ee0a143490cba30",
+    "url": "/ponce-char-project-5/static/media/watering-can.46420a8e.png"
+  }
+]);
